021ef23bcc1d82cfc51449d6a008a9934f4a7b9b
Zia-Ul-Qamar-Tech-Notes <zia.softwareprogrammer@gmail.com>
2025-02-27 12:38:15 +0500

    check

--- backend/serverless.js
+++ backend/serverless.js
@@ -1,3 +1,31 @@
+// // server init
+// const server = express();
+
+// // database connection
+// connectToDB();
+
+// // middlewares
+// server.use(
+//   cors({
+//     origin: process.env.ORIGIN,
+//     credentials: true,
+//     exposedHeaders: ["X-Total-Count"],
+//     methods: ["GET", "POST", "PATCH", "DELETE"],
+//   })
+// );
+// server.use(express.json());
+// server.use(cookieParser());
+// server.use(morgan("tiny"));
+
+// server.get("/", (req, res) => {
+//   res.status(200).json({ message: "running" });
+// });
+
+// server.listen(8000, () => {
+//   console.log("server [STARTED] ~ http://localhost:8000");
+// });
+
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
@@ -34,17 +62,18 @@ server.use(cookieParser());
 server.use(morgan("tiny"));
 
 // routes
-server.use("/api/auth", authRoutes);
-server.use("/api/users", userRoutes);
-server.use("/api/products", productRoutes);
-server.use("/api/orders", orderRoutes);
-server.use("/api/cart", cartRoutes);
-server.use("/api/brands", brandRoutes);
-server.use("/api/categories", categoryRoutes);
-server.use("/api/address", addressRoutes);
-server.use("/api/reviews", reviewRoutes);
-server.use("/api/wishlist", wishlistRoutes);
-// server.use("/api/search", search);
+// routeMiddleware
+server.use("/auth", authRoutes);
+server.use("/users", userRoutes);
+server.use("/products", productRoutes);
+server.use("/orders", orderRoutes);
+server.use("/cart", cartRoutes);
+server.use("/brands", brandRoutes);
+server.use("/categories", categoryRoutes);
+server.use("/address", addressRoutes);
+server.use("/reviews", reviewRoutes);
+server.use("/wishlist", wishlistRoutes);
+// server.use("/search", search);
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: "running" });
